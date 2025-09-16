6b6d44e2ba02716e6ea1ea322fc39d9bf4100119
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-09-16 15:52:53 +0530

    js

--- Javascript/ex.js
+++ Javascript/ex.js
@@ -1,1 +1,13 @@
-console.log("HEllo external js");
+// console.log("HEllo external js");
+
+// bom :- browser object model 
+// pop box
+
+// alert("pls required form..!")
+// confirm("Are you sure ?")
+// prompt("5+ 5 => ?")
+// print()
+
+
+// innerHeight()
+// innerWidth()
